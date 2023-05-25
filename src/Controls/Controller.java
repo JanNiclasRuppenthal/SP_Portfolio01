c89122fca7bd941e8a3eaa1bb3645c6420cc7c20
JanNiclasRuppenthal <jan.rupp06@web.de>
2023-05-25 15:37:51 +0200

    Improved controls

--- src/Controls/Controller.java
+++ src/Controls/Controller.java
@@ -13,6 +13,7 @@ public class Controller
     public static void addControls(Stage primaryStage, Group landscape, Camera camera)
     {
         primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            Sphere sphere = null;
             switch (event.getCode()) {
                 case W:
 
@@ -27,7 +28,6 @@ public class Controller
                     Landscape.player.setTranslateY(500);
 
                     // Überprüfe den Schnitt mit anderen Kugeln
-                    Sphere sphere = null;
                     for (Node hill : landscape.getChildren())
                     {
                         if (hill instanceof Sphere && hill != Landscape.player)
@@ -44,9 +44,28 @@ public class Controller
 
                     break;
                 case S:
-                    landscape.translateZProperty().set(landscape.getTranslateZ() + 100);
+                    camera.translateZProperty().set(camera.getTranslateZ() - 100);
                     Landscape.player.translateZProperty().set(Landscape.player.getTranslateZ() - 100);
                     Landscape.pointLight.translateZProperty().set(Landscape.pointLight.getTranslateZ() - 100);
+
+                    // Kein Schnitt, bewege den Spieler auf den Boden
+                    Landscape.player.setTranslateY(500);
+
+                    // Überprüfe den Schnitt mit anderen Kugeln
+                    for (Node hill : landscape.getChildren())
+                    {
+                        if (hill instanceof Sphere && hill != Landscape.player)
+                        {
+                            sphere = (Sphere) hill;
+                            if (sphere.getBoundsInParent().intersects(Landscape.player.getBoundsInParent()))
+                            {
+                                // Es gibt einen Schnitt, lasse den Spieler über der Kugel schweben
+                                Landscape.player.setTranslateY(sphere.getTranslateY() - sphere.getRadius() - 100);
+                                break;
+                            }
+                        }
+                    }
+
                     break;
             }
         });
